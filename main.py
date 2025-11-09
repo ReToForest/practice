00c956e1b79c5e8575d0047833b63e8a7ad9049e
ReToForest <3548645094@qq.com>
2025-11-09 15:09:10 +0800

    improve our game

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 temp=input("不妨猜一下我现在心里想的是哪个数字:")
 guess=int(temp)
-
 if guess==8:
     print("yes")
     print("but there has no price")
+    print("game over")
 else:
-    print("no")
+    if guess < 8:
+        print("小了")
+    else:    
+        print("大了")
 
 print("game over")
         
